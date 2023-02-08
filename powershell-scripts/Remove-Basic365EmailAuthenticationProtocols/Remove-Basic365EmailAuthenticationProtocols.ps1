d872436b64e76bee2fcc04bd462e999f5bd506b8
Emer <emerconn7@gmail.com>
2023-02-07 23:40:28 -0600

    first commit

---
+++ powershell-scripts/Remove-Basic365EmailAuthenticationProtocols/Remove-Basic365EmailAuthenticationProtocols.ps1
@@ -0,0 +1,8 @@
+$mailboxes = Get-Mailbox -ResultSize unlimited
+$count = 1
+$total = ($mailboxes | Measure-Object).Count
+foreach ($mailbox in $mailboxes) {
+  Write-Host "($count/$total) $($mailbox.UserPrincipalName)"
+  $count++
+  Set-CasMailbox -Identity $mailbox.Guid -PopEnabled $false -ImapEnabled $false -ActiveSyncEnabled $false -SmtpClientAuthenticationDisabled $true -EwsEnabled $false
+}

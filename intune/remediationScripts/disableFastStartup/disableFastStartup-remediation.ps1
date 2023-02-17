fc3ef5841f9e6bcaaa56f32442bea0f15a1675f5
Emer <emerconn7@gmail.com>
2023-02-16 23:48:11 -0600

    Update disableFastStartup-remediation.ps1

--- intune/remediationScripts/disableFastStartup/disableFastStartup-remediation.ps1
+++ intune/remediationScripts/disableFastStartup/disableFastStartup-remediation.ps1
@@ -1,5 +1,3 @@
 $PowerRegPath = 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power'
 $PowerRegName = 'HiberbootEnabled'
-if (((Get-ItemProperty -Path $PowerRegPath -Name $PowerRegName).$PowerRegName) -ne 0) {
-    Set-ItemProperty -Path $PowerRegPath -Name $PowerRegName -Value 0 -Force -Confirm:$false
-}
+Set-ItemProperty -Path $PowerRegPath -Name $PowerRegName -Value 0 -Force -Confirm:$false
